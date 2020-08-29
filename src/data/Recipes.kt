977d333ff2f82895c26a2a513110006b1c654048
SigismundSchlomo <kuzenstein23@gmail.com>
2020-08-29 20:00:05 +0300

    Exposed database interface implemented

---
+++ src/data/Recipes.kt
@@ -0,0 +1,12 @@
+package com.sigismund.data
+
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IntIdTable
+
+
+object RecipesTable : IntIdTable() {
+    val header = varchar("header", 70)
+    val body = varchar("body", 512)
+}
