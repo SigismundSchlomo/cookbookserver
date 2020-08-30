46554350b376662f325bf638a9a5728bfc4d77c9
SigismundSchlomo <kuzenstein23@gmail.com>
2020-08-30 21:10:04 +0300

    Endpoints implemented

--- src/data/RecipeRepository.kt
+++ src/data/RecipeRepository.kt
@@ -5,10 +5,10 @@ import org.jetbrains.exposed.dao.id.EntityID
 
 interface RecipeRepository {
 
-    suspend fun addRecipe(header: String, body: String): EntityID<Int>
+    suspend fun addRecipe(recipe: Recipe): EntityID<Int>?
     suspend fun deleteRecipe(id: Int)
     suspend fun getRecipes(): List<Recipe>
     suspend fun findRecipe(id: Int): Recipe
-    suspend fun updateRecipe(id: Int, header: String, body: String)
+    suspend fun updateRecipe(recipe: Recipe, id: Int)
 
 }
--- src/data/models/Recipe.kt
+++ src/data/models/Recipe.kt
@@ -1,7 +1,7 @@
 package com.sigismund.data.models
 
 data class Recipe(
-    val id: Int,
+    val id: Int = -1,
     val header: String,
     val body: String
 )
