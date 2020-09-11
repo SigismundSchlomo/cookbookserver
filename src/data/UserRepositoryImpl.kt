38a7a8d5acc4dc4372bfc06584b6ec3fb7737895
SigismundSchlomo <kuzenstein23@gmail.com>
2020-09-11 18:05:58 +0300

    User routes implemented

---
+++ src/data/UserRepositoryImpl.kt
@@ -0,0 +1,56 @@
+package com.sigismund.data
+
+import com.sigismund.data.DatabaseFactory.dbQuery
+import com.sigismund.data.models.User
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.deleteWhere
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.statements.InsertStatement
+
+class UserRepositoryImpl : UserRepository {
+
+    override suspend fun addUser(email: String, displayName: String, passwordHash: String): User? {
+        var statement: InsertStatement<Number>? = null
+        dbQuery {
+            statement = Users.insert {
+                it[Users.email] = email
+                it[Users.displayName] = displayName
+                it[Users.passwordHash] = passwordHash
+            }
+        }
+        return rowToUser(statement?.resultedValues?.get(0))
+    }
+
+    override suspend fun deleteUser(userId: Int) {
+        dbQuery {
+            Users.deleteWhere {
+                Users.id.eq(userId)
+            }
+        }
+    }
+
+    override suspend fun findUser(userId: Int) = dbQuery {
+        Users.select { Users.id.eq(userId) }
+            .map { rowToUser(it) }.singleOrNull()
+    }
+
+    override suspend fun findUserByEmail(email: String) = dbQuery {
+        Users.select { Users.email.eq(email) }
+            .map { rowToUser(it) }.singleOrNull()
+    }
+
+
+    private fun rowToUser(row: ResultRow?): User? {
+        if (row == null) {
+            return null
+        }
+        return User(
+            userId = row[Users.id].value,
+            email = row[Users.email],
+            displayName = row[Users.displayName],
+            passwordHash = row[Users.passwordHash]
+        )
+    }
+
+}
