3a1f9594fbff6991148ae53da0652644fe47c774
SigismundSchlomo <kuzenstein23@gmail.com>
2020-09-27 18:22:44 +0300

    Ingredient data source updated

--- src/data/ingredient/IngredientDataSourceImpl.kt
+++ src/data/ingredient/IngredientDataSourceImpl.kt
@@ -1,16 +1,57 @@
 package com.sigismund.data.ingredient
 
 import com.sigismund.data.DatabaseFactory
+import com.sigismund.data.DatabaseFactory.dbQuery
 import com.sigismund.domain.data.sources.IngredientDataSource
 import com.sigismund.models.Ingredient
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.sql.*
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 
 class IngredientDataSourceImpl : IngredientDataSource {
 
+    override suspend fun getIngredients(recipeId: Int): List<Ingredient> {
+        return dbQuery {
+            Ingredients.select {
+                Ingredients.recipeId eq recipeId
+            }.mapNotNull { row ->
+                rowToIngredient(row)
+            }
+        }
+    }
+
+    override suspend fun deleteIngredients(recipeId: Int) {
+        dbQuery { Ingredients.deleteWhere { Ingredients.recipeId eq recipeId } }
+    }
+
+    override suspend fun insertIngredients(ingredients: List<Ingredient>) {
+        ingredients.forEach { ingredient ->
+            dbQuery {
+                Ingredients.insert { ingredients ->
+                    ingredients[userId] = ingredient.userId
+                    ingredients[recipeId] = ingredient.recipeId
+                    ingredients[name] = ingredient.name
+                    ingredients[quantity] = ingredient.quantity
+                    ingredients[isInTheList] = ingredient.isInTheList
+                }
+            }
+        }
+    }
+
+    override suspend fun updateIngredients(ingredients: List<Ingredient>, recipeId: Int) {
+        ingredients.forEach {ingredient ->
+            dbQuery {
+                Ingredients.update ({Ingredients.recipeId eq recipeId}) {ingredients ->
+                    ingredients[name] = ingredient.name
+                    ingredients[quantity] = ingredient.quantity
+                    ingredients[isInTheList] = ingredient.isInTheList
+                }
+            }
+
+        }
+    }
+
     override suspend fun getIngredientsInShoppingList(userId: Int): List<Ingredient> {
-        return DatabaseFactory.dbQuery {
+        return dbQuery {
             Ingredients.select {
                 Ingredients.userId.eq(userId)
             }.mapNotNull { row ->
@@ -21,7 +62,7 @@ class IngredientDataSourceImpl : IngredientDataSource {
 
     override suspend fun createIngredient(ingredient: Ingredient): EntityID<Int>? {
         var id: EntityID<Int>? = null
-        DatabaseFactory.dbQuery {
+        dbQuery {
             id = Ingredients.insertAndGetId { ingredients ->
                 ingredients[userId] = ingredient.userId
                 ingredients[recipeId] = ingredient.recipeId
@@ -34,15 +75,15 @@ class IngredientDataSourceImpl : IngredientDataSource {
     }
 
     override suspend fun deleteIngredient(ingredientId: Int) {
-        DatabaseFactory.dbQuery { Ingredients.deleteWhere { Ingredients.id eq ingredientId } }
+        dbQuery { Ingredients.deleteWhere { Ingredients.id eq ingredientId } }
     }
 
     override suspend fun chekAsInList(ingredientId: Int) {
-        DatabaseFactory.dbQuery { Ingredients.update({ Ingredients.id eq ingredientId }) { it[isInTheList] = true } }
+        dbQuery { Ingredients.update({ Ingredients.id eq ingredientId }) { it[isInTheList] = true } }
     }
 
     override suspend fun uncheckAsInTheList(ingredientId: Int) {
-        DatabaseFactory.dbQuery { Ingredients.update({ Ingredients.id eq ingredientId }) { it[isInTheList] = false } }
+        dbQuery { Ingredients.update({ Ingredients.id eq ingredientId }) { it[isInTheList] = false } }
     }
 
     private fun rowToIngredient(row: ResultRow?): Ingredient? {
--- src/domain/data/sources/IngredientDataSource.kt
+++ src/domain/data/sources/IngredientDataSource.kt
@@ -5,10 +5,13 @@ import org.jetbrains.exposed.dao.id.EntityID
 
 interface IngredientDataSource {
 
+    suspend fun getIngredients(recipeId: Int): List<Ingredient>
+    suspend fun deleteIngredients(recipeId: Int)
+    suspend fun insertIngredients(ingredients: List<Ingredient>)
+    suspend fun updateIngredients(ingredients: List<Ingredient>, recipeId: Int)
     suspend fun getIngredientsInShoppingList(userId: Int): List<Ingredient>
     suspend fun createIngredient(ingredient: Ingredient): EntityID<Int>?
     suspend fun deleteIngredient(ingredientId: Int)
     suspend fun chekAsInList(ingredientId: Int)
     suspend fun uncheckAsInTheList(ingredientId: Int)
-
 }
